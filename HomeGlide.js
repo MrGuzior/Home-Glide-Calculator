45f640e1e2dab57fb608b645d8523fb9d878901d
MrGuzior <konrad.guzior@gmail.com>
2018-02-27 00:29:56 +0100

    Revert "1.1.1"
    This reverts commit a3a715cdd7db531725cee1eaa744327482fe3292.

--- HomeGlide.js
+++ HomeGlide.js
@@ -1,8 +1,16 @@
-										/////////////////////////////
-										//////////MrGuzior///////////
-										///////////2018//////////////
+/*function glideR(distance, height, modell){
+		let glideRacio = modell * height;
+		
 
-										
+		if (glideRacio>= distance) {
+			console.log("Go home!");
+					}
+		else if (glideRacio< distance) {
+			console.log("No Home!");
+		}
+}
+glideR(79, 2, 40);
+*/
 										// Getting the information
 import * from "gliderlist.js";
 
@@ -11,18 +19,21 @@ import * from "gliderlist.js";
 
 		function changeGlider(event){
 			glideRacio=event.value;
+			console.log(glideRacio);
 		}
 
 		let glideHeight = 0;
 
 		function heightGlider(event){
 			glideHeight=event.value;
+			console.log(glideHeight);
 		}
 
 		let glideDistance = 0;
 
 		function distanceGlider(event){
 			glideDistance=event.value;
+			console.log(glideDistance);
 		}
 
 		let glideSpeed = 0;
@@ -74,9 +85,10 @@ import * from "gliderlist.js";
 			homeHeight = 0;
 			homeHeight = glideDistance / glideRacio;
 			homeHeight = glideHeight-(homeHeight * 1000);
+			console.log(homeHeight.toFixed(0)+"m");
 			return homeHeight.toFixed(0)+"m";
 		}
-										//Showing results
+
 		function result(){
 		let result = document.getElementById("result")
 		result.innerHTML = homeComing(glideRatio, glideHeight, glideDistance, result, homeHeight);
